d072c6624b967e52555289a745ee44e1fbf3662f
GitLqr <960432662@qq.com>
2024-06-05 23:50:38 +0800

    feat: 实现图片选择功能

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+interface ImgFile {
+  path: string; //	本地文件路径
+  size: number; // 本地文件大小，单位：B
+  name?: string; // 包含扩展名的文件名称，仅H5支持
+  type?: string; // 文件类型，仅H5支持
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,79 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view class="index-container">
+    <view class="img-box" @click="onPickImg">
+      <view v-if="!img" class="add">+</view>
+      <image v-if="img" class="img" :src="img.path" mode="aspectFit" />
     </view>
+    <view class="gen-btn" @click="onGenImg">生成证件照</view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from "vue";
+
+const img = ref<ImgFile>();
+
+function onPickImg() {
+  uni.chooseImage({
+    count: 1,
+    sizeType: ["original", "compressed"],
+    sourceType: ["album", "camera"],
+    success: (res) => {
+      if (res.tempFiles instanceof Array) {
+        img.value = res.tempFiles[0] as any;
+      } else {
+        img.value = res.tempFiles as any;
+      }
+    },
+    fail: (error) => {
+      console.log(error);
+      uni.showToast({
+        title: "pick img fail",
+        icon: "error",
+        mask: true,
+      });
+    },
+  });
+}
+
+function onGenImg() {}
 </script>
 
-<style>
-.content {
+<style lang="scss">
+.index-container {
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  .img-box {
+    margin-top: 40rpx;
+    width: 400rpx;
+    height: 400rpx;
+    background-color: #e7e7e7;
+    .add {
+      width: 100%;
+      height: 100%;
+      color: white;
+      font-size: 100rpx;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .gen-btn {
+    margin-top: 20rpx;
+    width: 400rpx;
+    height: 80rpx;
+    border-radius: 10rpx;
+    color: white;
+    background-color: #22a5f1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
