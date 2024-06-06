05afc88d9d2214a27d383c1fc60d14548acaf821
GitLqr <960432662@qq.com>
2024-06-06 17:50:02 +0800

    feat: 实现 canvas 绘制证件照效果

--- src/components/photo-dialog/photo-dialog.vue
+++ src/components/photo-dialog/photo-dialog.vue
@@ -2,13 +2,24 @@
   <uni-popup ref="popup" type="dialog">
     <view class="photo-dialog">
       <!-- 证件照 -->
-      <image
+      <!-- <image
         :src="img"
         mode="scaleToFill"
         :style="{
           backgroundColor: imgBg,
         }"
+      /> -->
+
+      <canvas
+        :style="{
+          width: `${canvasWidth}px`,
+          height: `${canvasHeight}px`,
+        }"
+        :canvas-id="canvasId"
+        :id="canvasId"
+        @error="onCanvasError"
       />
+
       <!-- 颜色列表 -->
       <view class="color-list">
         <view
@@ -25,26 +36,58 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { getCurrentInstance, ref, watch } from "vue";
 const props = defineProps();
 
 const popup = ref();
+const currentInstance = getCurrentInstance();
 
+const canvasId = "imgCanvas";
+const canvasWidth = uni.upx2px(400);
+const canvasHeight = uni.upx2px(500);
 const colorList = ref(["#FF0000", "#438EDB", "#FFFFFF"]);
 const img = ref();
 const imgBg = ref(colorList.value[0]);
+watch([img, imgBg], () => {
+  if (img.value && imgBg.value) {
+    // 需要延时保证 canvas 加载完成
+    setTimeout(() => {
+      loadImgCanvas();
+    }, 500);
+  }
+});
+
+function loadImgCanvas() {
+  const bgColor = imgBg.value;
+  console.log(
+    `loadImgCanvas(), bgColor = ${bgColor}, canvasSize = (${canvasWidth}, ${canvasHeight})`
+  );
+  const ctx = uni.createCanvasContext(canvasId, currentInstance);
+  // 绘制背景
+  ctx.setFillStyle(bgColor);
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  // 绘制人像
+  ctx.drawImage(img.value, 0, 0, canvasWidth, canvasHeight);
+  // 画
+  ctx.draw();
+}
+
+function onCanvasError(e: any) {
+  console.error(e.detail.errMsg);
+}
 
 function onColorClick(color: string) {
   imgBg.value = color;
 }
 
 function show(resImg: string) {
-  img.value = resImg;
   popup.value.open();
+  img.value = resImg;
 }
 
 function dismiss() {
   popup.value.close();
+  img.value = undefined;
 }
 
 defineExpose({
